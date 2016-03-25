c2f3aaf6e8b765bc53732af3777b232327e267fb
Aleksey Lobanov <alekseylobanov1@gmail.com>
2016-03-25 19:08:50 +0300

    Added basic tests for GetKeyNumsFromOutput

---
+++ src/main_test.go
@@ -0,0 +1,32 @@
+// main_test.go
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetKeyNumsFromOutput(t *testing.T) {
+	var buf []byte // = {"key press   41k ey press   41"}
+	var keyNums []uint8
+
+	const test1 = "key press 36\nkey release 41\nkey press   41"
+	var result1 = []uint8{36, 41}
+
+	const test2 = ""
+	var result2 = []uint8{}
+
+	// Test1. Simple
+	buf = []byte(test1)
+	keyNums = GetKeyNumsFromOutput(buf)
+	if !reflect.DeepEqual(keyNums, result1) {
+		t.Fail()
+	}
+
+	// Test2. Clear
+	buf = []byte(test2)
+	keyNums = GetKeyNumsFromOutput(buf)
+	if !reflect.DeepEqual(keyNums, result2) {
+		t.Fail()
+	}
+}
